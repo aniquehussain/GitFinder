ea817e5db110e35822b6ec6c599cbd64ebd96ba2
aniquehussain <47793113+aniquehussain@users.noreply.github.com>
2020-09-26 16:16:45 +0530

    version 1.0.1

--- src/Components/Routes/About.jsx
+++ src/Components/Routes/About.jsx
@@ -6,6 +6,7 @@ const About = () => {
             <h1>About</h1>
             <p>An app to find GitHub users</p>
             <p>Version 1.0.0</p>
+            <p>Developed by <a href="https://syedanique.herokuapp.com/">Syed Anique Hussain</a></p>
         </div>
     )
 }
